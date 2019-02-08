db636a2b63770d7d78ac8f6976d262119e657481
Michael Büchner <m.buechner@dnb.de>
2019-02-08 14:44:37 +0100

    Fixed Reverse Proxy Settings

--- DDBmeme/ddbmeme/settings.py
+++ DDBmeme/ddbmeme/settings.py
@@ -23,12 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cqIw.Cc`KH<"r"HR0=H3rw,*E#z?NqkQIS-bRT:UeBG&Fylz5(,m[s:<SB7ijW)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '0.0.0.0',
+    'labs.ddb.de',
+    'userddb.user.dnb.de',
 ]
 
 
