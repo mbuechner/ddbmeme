4a51ffac14ff75d088f09d413bfc5636f225d5f8
Michael Büchner <m.buechner@dnb.de>
2019-02-11 16:36:13 +0100

    Improved Dockerization and Reverse Proxy settings

--- DDBmeme/ddbmeme/urls.py
+++ DDBmeme/ddbmeme/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
 from ddbmeme.views import Search, autocompleteModel, maketextModel, makememeModel
-# PersonListView, PersonCreateView, PersonUpdateView
 
 urlpatterns = [
     path('', Search.as_view(), name='search_base'),
-    path('load/', autocompleteModel, name='autocompleteModel'),
-    path('url/', maketextModel, name='maketextModel'),
-    path('meme/', makememeModel, name='makememeModel'),
+    path('load', autocompleteModel, name='autocompleteModel'),
+    path('url', maketextModel, name='maketextModel'),
+    path('meme', makememeModel, name='makememeModel'),
 ]
